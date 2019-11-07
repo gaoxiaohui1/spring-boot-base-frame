82ccdcbd4617f3615f960ac1150d3e39a7b7e67c
gaoxiaohui <gaoxiaohui@fang.com>
2019-11-07 14:16:05 +0800

    controller参数校验

--- base_frame_api/src/main/java/com/base/frame/api/controller/OrderController.java
+++ base_frame_api/src/main/java/com/base/frame/api/controller/OrderController.java
@@ -7,12 +7,14 @@ import com.base.frame.model.entity.OrderEntity;
 import com.base.frame.service.order.OrderService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.Valid;
+
 
 /**
  * 订单
@@ -45,6 +47,24 @@ public class OrderController {
     @ApiOperation(value = "复杂参数校验", notes = "复杂参数校验")
     @RequestMapping(path = "validateParam", method = RequestMethod.POST)
     public BaseResult<OrderCreateInput> validateParam(@RequestBody OrderCreateInput orderCreateInput) {
+        BaseResult<OrderCreateInput> result = orderService.createOrder(orderCreateInput);
+        return result;
+    }
+
+    /**
+     * 复杂参数校验
+     *
+     * @param orderCreateInput
+     * @return
+     */
+    @ApiOperation(value = "复杂参数校验", notes = "复杂参数校验")
+    @RequestMapping(path = "validateParamInController", method = RequestMethod.POST)
+    public BaseResult<OrderCreateInput> validateParamInController(@RequestBody @Valid OrderCreateInput orderCreateInput, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            BaseResult<OrderCreateInput> result = ResultGenerator.fail(bindingResult.getFieldError().getDefaultMessage());
+            result.setCode(400);
+            return result;
+        }
         BaseResult<OrderCreateInput> result = orderService.createOrder(orderCreateInput);
         return result;
     }
