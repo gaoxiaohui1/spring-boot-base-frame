4e0aab2e20d16342e6542ac311e50a4c5bf94fbd
gaoxiaohui <gaoxiaohui@fang.com>
2020-03-31 15:35:17 +0800

    rabbit mq

---
+++ base_frame_consumer/src/main/java/com/base/frame/consumer/BaseFrameConsumerApplication.java
@@ -0,0 +1,21 @@
+package com.base.frame.consumer;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@ServletComponentScan
+@ComponentScan("com.base.frame")
+@MapperScan("com.base.frame.dao")
+@EnableTransactionManagement
+public class BaseFrameConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseFrameConsumerApplication.class, args);
+    }
+
+}
