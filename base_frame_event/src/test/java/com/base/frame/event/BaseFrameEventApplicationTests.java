6dcaf175e9409ea90a088b68f4cc44492dff2a15
gaoxiaohui <gaoxiaohui@fang.com>
2020-08-06 17:34:57 +0800

    Apollo分布式配置相关 https://github.com/ctripcorp/apollo/wiki/%E5%88%86%E5%B8%83%E5%BC%8F%E9%83%A8%E7%BD%B2%E6%8C%87%E5%8D%97 https://hub.docker.com/repositories

---
+++ base_frame_event/src/test/java/com/base/frame/event/BaseFrameEventApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.base.frame.base_frame_event;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BaseFrameEventApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
