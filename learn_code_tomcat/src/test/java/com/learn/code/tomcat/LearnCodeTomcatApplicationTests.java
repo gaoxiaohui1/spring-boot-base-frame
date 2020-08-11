01fdcf8fdf8fd9f25e8c89a9a6dcbcd88e7754d2
gaoxiaohui <gaoxiaohui@fang.com>
2020-08-11 14:19:18 +0800

    https://mp.weixin.qq.com/s/JDcmMsc8ntyd_3qbGvkROg

---
+++ learn_code_tomcat/src/test/java/com/learn/code/tomcat/LearnCodeTomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.code.tomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnCodeTomcatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
